a0a8b79ba323f6a585ec533f39ddb3e98b0514f8
Imed Boumalek <imedboumalek@gmail.com>
2022-01-18 22:27:57 +0100

    changed project to ts

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,17 @@
-import createError from "http-errors";
 import express from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import path from "path";
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+app.listen(process.env.PORT ?? 8080, () => {
+  console.log(`Server is running on port ${process.env.PORT ?? 8080}`);
 });
 
 module.exports = app;
