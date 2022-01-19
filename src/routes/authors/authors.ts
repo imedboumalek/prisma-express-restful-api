09ebcd338103085ca9e5d58a516ba354e5e67a32
Imed Boumalek <imedboumalek@gmail.com>
2022-01-19 19:44:35 +0100

    made get user with jwt

--- src/routes/authors/authors.ts
+++ src/routes/authors/authors.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import express from "express";
+import jwt from "jsonwebtoken";
 import dbclient from "../../prisma-client";
 const authorsRouter = express.Router();
 authorsRouter.get("/authors", (req, res) => {
@@ -29,7 +30,38 @@ authorsRouter.get("/authors", (req, res) => {
     });
 });
 authorsRouter.get("/authors/current", (req, res) => {
-  res.send("respond with a resource");
+  // validate headers
+  const auth = req.headers.authorization;
+  if (!auth) {
+    res.status(401).json({
+      message: "Unauthorized",
+    });
+    return;
+  }
+  const token = auth.split(" ")[1];
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      res.status(401).json({
+        message: "Unauthorized",
+      });
+      return;
+    }
+    dbclient.author
+      .findFirst({
+        where: {
+          username: decoded["username"],
+        },
+        include: {
+          country: true,
+          org: true,
+          submissions: true,
+        },
+      })
+      .then((author) => {
+        const { salt, password, jwt, ...theRest } = author;
+        res.json(theRest);
+      });
+  });
 });
 
 export default authorsRouter;
