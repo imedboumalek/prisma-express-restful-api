3df059ef4c5402020646def5f44f12c5083f61d7
Imed Boumalek <imedboumalek@gmail.com>
2022-01-29 17:34:44 +0100

    removed jwt from db

---
+++ prisma/migrations/20220129163431_remove_jwt/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `jwt` on the `Author` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Author" DROP COLUMN "jwt";
