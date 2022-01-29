c28d9a5796509f7b2312c00ac96d4ed291255d2a
Imed Boumalek <imedboumalek@gmail.com>
2022-01-29 18:59:40 +0100

    refactored auth

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -2,8 +2,9 @@ import express from "express";
 import dbclient from "../prisma-client";
 import bcrypt from "bcrypt";
 import jsonwebtoken from "jsonwebtoken";
+import { Author } from "@prisma/client";
 const router = express.Router();
-const checkRequiredFields = (req, res, next) => {
+const checkRequiredFieldsForSignIn = (req, res, next) => {
   console.log("login");
 
   const { username, password } = req.body;
@@ -18,33 +19,42 @@ const checkRequiredFields = (req, res, next) => {
   }
   next();
 };
-//TODO refactor this
-router.post("/login", checkRequiredFields, async (req, res) => {
-  const { username, password } = req.body;
 
+const checkUserExistance = async (req, res, next) => {
+  const { username } = req.body;
   const user = await dbclient.author.findFirst({
     where: {
       username: username,
     },
   });
-  console.log("user", user);
-
   if (!user) {
-    res.status(400).json({
-      message: "Username or password is incorrect",
+    res.status(403).json({
+      message: "Invalid username or password",
     });
     return;
   }
+  res.locals.user = user;
+  next();
+};
+const checkPassword = async (req, res, next) => {
+  const { password } = req.body;
+  const user: Author = res.locals.user;
   const hashedPassword = await bcrypt.hash(password, user.salt);
   const isPasswordValid = hashedPassword === user.password;
   console.log("isPasswordValid", isPasswordValid);
 
   if (!isPasswordValid) {
-    res.status(400).json({
+    res.status(403).json({
       message: "Username or password is incorrect",
     });
     return;
   }
+
+  next();
+};
+
+const returnJWT = async (req, res) => {
+  const user: Author = res.locals.user;
   const token = jsonwebtoken.sign(
     {
       sub: user.id,
@@ -57,7 +67,15 @@ router.post("/login", checkRequiredFields, async (req, res) => {
   res.status(200).json({
     access_token: token,
   });
-});
+};
+
+router.post(
+  "/login",
+  checkRequiredFieldsForSignIn,
+  checkUserExistance,
+  checkPassword,
+  returnJWT
+);
 
 router.all("/login", (req, res) => {
   // return method not allowed
